1d015bd7a50ec1bffcd5540d671f5bf875e0be8d
Tirth Jain <jaintirth2610@gmail.com>
2025-03-24 09:33:44 +0530

    added

---
+++ array/medium/count_days_without_meetings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) 
+    {
+        sort(begin(meetings),end(meetings));
+
+        int result=0;
+        int end=0;
+
+        for(auto &meet : meetings)
+        {
+            if(meet[0]>end)
+            {
+                result+=meet[0]-end-1;
+            }
+            end=max(meet[1],end);
+        }  
+        if(days>end)
+        {
+            result+=days-end;
+        }  
+        return result;
+    }
+};
