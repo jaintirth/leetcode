1d080589679fbedb8ecfc225190de687a6d7d96d
Tirth Jain <jaintirth2610@gmail.com>
2025-04-13 21:13:39 +0530

    converted a non-optimal soln to optimal

--- math/medium/pow.cpp
+++ math/medium/pow.cpp
@@ -1,31 +1,33 @@
 class Solution {
-public:
-    double myPow(double x, int n) 
-    {
-        int m=n;
-        if(n<0)
-        {
-            m=-n;
-        }
-        if(m==1)
-        {
-            return x;
-        }
-        else if(m==0)
-        {
+public: 
+
+    double calculatePow(double x, int n){
+        if(n==0){
             return 1;
         }
-        else
-        {
-            double res = x*myPow(x,m-1); 
-            if(n>0)
-            {
-                return res;
-            }
-            else
-            {
-                return 1/res;
-            }
+        if(n==1){
+            return x;
         }
+        bool isEven = true;
+        if(n%2){
+            isEven = false;
+        }
+        double half = calculatePow(x,n/2);
+        double res = half*half;
+        if(!isEven){
+            res *= x;
+        }
+        return res;
+    }
+
+
+    double myPow(double x, int n) {
+        long long N = n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+       return calculatePow(x,N);
     }
 };
+
