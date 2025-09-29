8d6c6b5e8e2eac474080384bb6fb41f078968039
Tirth Jain <jaintirth2610@gmail.com>
2025-09-29 17:40:26 +0530

    potd

---
+++ dynamic_programming/medium/minimum_score_triangulation_of_polygon.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int t[51][51];  //memoization , if the same case repeats then we will return the stored result
+    int solve(vector<int>& values, int i, int j)
+    {
+        if(j-i+1<3)
+        {
+            return 0;
+        }
+        if(t[i][j]!=-1){
+            return t[i][j];
+        }
+        int result=INT_MAX;
+
+        for(int k=i+1;k<j;k++)
+        {
+            int wt=values[i]*values[j]*values[k]
+                    + solve(values, i, k)
+                    + solve(values, k, j);
+
+            result = min(result, wt);
+        }
+        return t[i][j]= result;
+    }
+
+    int minScoreTriangulation(vector<int>& values) {
+        int n=values.size();
+        memset(t, -1, sizeof(t));
+        return solve(values, 0, n-1);
+    }
+};
