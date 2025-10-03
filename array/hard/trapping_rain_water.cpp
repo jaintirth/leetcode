cf2b847b32634457701cbc2fdbff08f4cd2e3691
Tirth Jain <jaintirth2610@gmail.com>
2025-10-03 17:32:40 +0530

    random problem

---
+++ array/hard/trapping_rain_water.cpp
@@ -0,0 +1,42 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+
+    class Solution {
+    public:
+
+        vector<int> getLeftMax(vector<int>& height, int& n)
+        {
+            vector<int> leftMax(n);
+            leftMax[0]=height[0];
+            for(int i=1;i<n;i++)
+            {
+                leftMax[i]=max(leftMax[i-1], height[i]);
+            }
+            return leftMax;
+        }
+
+        vector<int> getRightMax(vector<int>& height, int& n)
+        {
+            vector<int> rightMax(n);
+            rightMax[n-1]=height[n-1];
+            for(int i=n-2;i>=0;i--)
+            {
+                rightMax[i]=max(height[i], rightMax[i+1]);
+            }
+            return rightMax;
+        }
+
+        int trap(vector<int>& height) {
+            int n=height.size();
+            vector<int> leftMax=getLeftMax(height, n);
+            vector<int> rightMax=getRightMax(height, n);
+
+            int result=0;
+            for(int i=0;i<n;i++)
+            {
+                int water_trapped=min(leftMax[i], rightMax[i])-height[i];
+                result+=water_trapped;
+            }
+            return result;
+        }
+    };
