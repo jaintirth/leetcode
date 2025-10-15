8a52e7f6b4d7018f01df42ed25c29f5e12704595
Tirth Jain <jaintirth2610@gmail.com>
2025-10-15 16:42:32 +0530

    potd

---
+++ array/medium/adjancent_increasing_subarray_detection_2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        int currRun=1;
+        int prevRun=0;
+        int k=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1]){
+                currRun++;
+            }else{
+                prevRun = currRun;
+                currRun=1;
+            }
+            k = max(k, min(currRun, prevRun));
+            k = max(k, currRun/2);
+        }
+        return k;        
+  }
+};
